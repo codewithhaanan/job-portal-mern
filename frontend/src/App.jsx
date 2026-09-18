fc2979a60b606701558b31f4537709bc6d8c3ac3
Hanan Dev <codewithhanaan@gmail.com>
2026-09-18 18:15:12 +0500

    Start working on frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/shared/Navbar'
+import { BrowserRouter } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter> <Navbar/></BrowserRouter>
+     
+      
+    </div>
+  )
+}
+
+export default App
